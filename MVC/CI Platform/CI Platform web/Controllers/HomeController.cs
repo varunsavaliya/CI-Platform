4156783941c097c513414943e29f231806f388e2
varunsavaliya <varunsavaliya000@gmail.com>
2023-02-13 15:34:58 +0530

    login, registration, forgot password, reset password views complete

---
+++ MVC/CI Platform/CI Platform web/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using CI_Platform_web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CI_Platform_web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        
+        public IActionResult Forgot_Password()
+        {
+            return View();
+        } 
+        
+        public IActionResult Reset_Password()
+        {
+            return View();
+        } 
+        
+        public IActionResult Registration()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
