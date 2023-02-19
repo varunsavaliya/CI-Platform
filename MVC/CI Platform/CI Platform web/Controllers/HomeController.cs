856bfcfaec078a7d73bd7c87ab49be87e80e5f19
varun <varunsavaliya000@gmail.com>
2023-02-19 21:44:22 +0530

    header and searchbar of landing page

--- MVC/CI Platform/CI Platform web/Controllers/HomeController.cs
+++ MVC/CI Platform/CI Platform web/Controllers/HomeController.cs
@@ -37,6 +37,10 @@ namespace CI_Platform_web.Controllers
         {
             return View();
         }
+        public IActionResult LandingPage()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ MVC/CI Platform/CI Platform web/wwwroot/js/SearchBar.js
@@ -0,0 +1,17 @@
+﻿// Close button functionality for every pills
+var closeButtons = document.querySelectorAll('.pill .close');
+var closeAllButton = document.getElementsByClassName('closeAll');
+for (var i = 0; i < closeButtons.length; i++) {
+    closeButtons[i].addEventListener('click', function () {
+        console.log(this.parentNode)
+
+        this.parentNode.style.display = 'none';
+    });
+}
+// Close button functionality for close all text
+closeAllButton[0].addEventListener('click', function () {
+    for (var i = 0; i < closeButtons.length; i++) {
+        closeButtons[i].parentNode.style.display = 'none';
+    }
+    this.style.display = "none";
+})
