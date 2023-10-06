d58a6b6217aca52a2e6721ca2b4b9733e847ad16
Varun Savaliya <122857288+varunsavaliya@users.noreply.github.com>
2023-10-06 22:20:17 +0530

    Delete HTML directory

--- HTML/js/reset-password.js
+++
@@ -1,8 +0,0 @@
-let password = document.querySelector(".new-pass");
-let confirmPassword = document.querySelector(".confirm-new-pass");
-
-confirmPassword.addEventListener("blur", function () {
-    if (password.value != confirmPassword.value) {
-        alert("Please enter same value for both passwords");
-    }
-})
